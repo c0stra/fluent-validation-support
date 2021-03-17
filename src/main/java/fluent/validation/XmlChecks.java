b5372a02fc5e9c6c00562bbc01c50a60a71ac947
c0stra <ondrej.fischer@gmail.com>
2021-03-18 00:14:46 +0100

    Complete XML checking with parsers.

--- src/main/java/fluent/validation/XmlChecks.java
+++ src/main/java/fluent/validation/XmlChecks.java
@@ -1,13 +1,20 @@
 package fluent.validation;
 
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
 import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.*;
+
+import java.io.StringReader;
 
 import static java.lang.Boolean.TRUE;
 
 public class XmlChecks {
 
     private static final XPath xPath = XPathFactory.newInstance().newXPath();
+    private static final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
     private static XPathExpression compile(String xPath) {
         try {
@@ -16,6 +23,7 @@ public class XmlChecks {
             throw new IllegalArgumentException(e);
         }
     }
+
     public static Check<Object> matchesXPath(String xPath) {
         XPathExpression xPathExpression = compile(xPath);
         return BasicChecks.check(xml -> TRUE.equals(xPathExpression.evaluate(xml, XPathConstants.BOOLEAN)), xPath);
@@ -33,6 +41,26 @@ public class XmlChecks {
     public static CheckBuilder<Object, Check<String>> hasXPath(String xPath) {
         XPathExpression xPathExpression = compile(xPath);
         return BasicChecks.has(xPath, xPathExpression::evaluate);
+    }
+
+    /**
+     * Use string parameter as XML content, and check the file using DOM Document check.
+     *
+     * @param check DOM Document check.
+     * @return String content check.
+     */
+    public static Check<String> asXml(Check<? super Document> check) {
+        return BasicChecks.transform(string -> factory.newDocumentBuilder().parse(new InputSource(new StringReader(string))), check);
+    }
+
+    /**
+     * Use string parameter as path to XML file, and check the file using DOM Document check.
+     *
+     * @param check DOM Document check.
+     * @return File name check.
+     */
+    public static Check<String> asXmlFile(Check<? super Document> check) {
+        return BasicChecks.transform(string -> factory.newDocumentBuilder().parse(string), check);
     }
 
 }
--- src/test/java/fluent/validation/tests/XmlChecksTest.java
+++ src/test/java/fluent/validation/tests/XmlChecksTest.java
@@ -2,11 +2,12 @@ package fluent.validation.tests;
 
 import fluent.validation.utils.Requirements;
 
-public class XmlChecksTest extends Requirements {
+import static fluent.validation.XmlChecks.asXml;
+import static fluent.validation.XmlChecks.matchesXPath;
 
-    {
-        //testOf("").using(matchesXPath("")).shouldReturn(false);
-        //testOf("").using(matchesXPath("")).shouldReturn(true);
-    }
+public class XmlChecksTest extends Requirements {{
 
-}
+    testOf("<root></root>").using(asXml(matchesXPath("/root"))).shouldReturn(true);
+    testOf("<root></root>").using(asXml(matchesXPath("/other"))).shouldReturn(false);
+
+}}
