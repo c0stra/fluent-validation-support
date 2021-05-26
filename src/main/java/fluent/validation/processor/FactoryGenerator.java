013553a0a07c74bdd9865d2dc4e6ff5267c8a52c
c0stra <ondrej.fischer@gmail.com>
2021-05-26 12:48:28 +0200

    Making copyrights consistent - sources with the LICENSE in the project metadata.

--- src/main/java/fluent/validation/processor/FactoryGenerator.java
+++ src/main/java/fluent/validation/processor/FactoryGenerator.java
@@ -1,26 +1,30 @@
 /*
- * Copyright © 2018 Ondrej Fischer. All Rights Reserved.
+ * BSD 2-Clause License
  *
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
- * the following conditions are met:
+ * Copyright (c) 2021, Ondrej Fischer
+ * All rights reserved.
  *
- * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
- *    following disclaimer.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
- *    following disclaimer in the documentation and/or other materials provided with the distribution.
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
  *
- * 3. The name of the author may not be used to endorse or promote products derived from this software without
- *     specific prior written permission.
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY [LICENSOR] "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
  */
 
 package fluent.validation.processor;
@@ -64,30 +68,33 @@ public class FactoryGenerator extends AbstractProcessor {
         }
         try(PrintWriter out = new PrintWriter(processingEnv.getFiler().createSourceFile(Check.class.getCanonicalName() + "s").openWriter())) {
             out.println("/*\n" +
-                    " * Copyright © 2018 Ondrej Fischer. All Rights Reserved.\n" +
+                    " * BSD 2-Clause License\n" +
                     " *\n" +
-                    " * Redistribution and use in source and binary forms, with or without modification, are permitted provided that\n" +
-                    " * the following conditions are met:\n" +
+                    " * Copyright (c) 2021, Ondrej Fischer\n" +
+                    " * All rights reserved.\n" +
                     " *\n" +
-                    " * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the\n" +
-                    " *    following disclaimer.\n" +
+                    " * Redistribution and use in source and binary forms, with or without\n" +
+                    " * modification, are permitted provided that the following conditions are met:\n" +
                     " *\n" +
-                    " * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the\n" +
-                    " *    following disclaimer in the documentation and/or other materials provided with the distribution.\n" +
+                    " *  Redistributions of source code must retain the above copyright notice, this\n" +
+                    " *   list of conditions and the following disclaimer.\n" +
                     " *\n" +
-                    " * 3. The name of the author may not be used to endorse or promote products derived from this software without\n" +
-                    " *     specific prior written permission.\n" +
+                    " *  Redistributions in binary form must reproduce the above copyright notice,\n" +
+                    " *   this list of conditions and the following disclaimer in the documentation\n" +
+                    " *   and/or other materials provided with the distribution.\n" +
                     " *\n" +
-                    " * THIS SOFTWARE IS PROVIDED BY [LICENSOR] \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT\n" +
-                    " * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE\n" +
-                    " * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,\n" +
-                    " * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE\n" +
-                    " * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON\n" +
-                    " * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR\n" +
-                    " * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF\n" +
-                    " * SUCH DAMAGE.\n" +
-                    " */\n" +
-                    "\n");
+                    " * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"\n" +
+                    " * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n" +
+                    " * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE\n" +
+                    " * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE\n" +
+                    " * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL\n" +
+                    " * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR\n" +
+                    " * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER\n" +
+                    " * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,\n" +
+                    " * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE\n" +
+                    " * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n" +
+                    " *\n" +
+                    " */\n");
             out.println("package fluent.validation;");
             out.println();
             out.println("/**\n" +
